e54e51f547047407b6f36ba5f9ddbbbae8870d2a
Karnakhall <emil_q21@wp.pl>
2025-05-30 20:57:35 +0200

    MeshComponent and SetupAttachment

--- Source/Praktyki/Private/Cars/PorscheCar.cpp
+++ Source/Praktyki/Private/Cars/PorscheCar.cpp
@@ -5,6 +5,119 @@
 
 APorscheCar::APorscheCar()
 {
+	CarSkeletalMesh = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("CarSkeletalMesh"));
+	SetRootComponent(CarSkeletalMesh);
+
+	MainBody = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("MainBody"));
+	MainBody->SetupAttachment(CarSkeletalMesh);
+
+	DoorLeft = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("DoorLeft"));
+	DoorLeft->SetupAttachment(CarSkeletalMesh);
+
+	DoorRight = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("DoorRight"));
+	DoorRight->SetupAttachment(CarSkeletalMesh);
+
+	Window = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Window"));
+	Window->SetupAttachment(CarSkeletalMesh);
+
+	FenderLeft = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("FenderLeft"));
+	FenderLeft->SetupAttachment(CarSkeletalMesh);
+
+	FenderRight = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("FenderRight"));
+	FenderRight->SetupAttachment(CarSkeletalMesh);
+
+	FrontBumper = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("FrontBumper"));
+	FrontBumper->SetupAttachment(CarSkeletalMesh);
+
+	FrontHood = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("FrontHood"));
+	FrontHood->SetupAttachment(CarSkeletalMesh);
+
+	RearBoot = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("RearBoot"));
+	RearBoot->SetupAttachment(CarSkeletalMesh);
+
+	RearBumper = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("RearBumper"));
+	RearBumper->SetupAttachment(CarSkeletalMesh);
+
+	RearSpoiler = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("RearSpoiler"));
+	RearSpoiler->SetupAttachment(CarSkeletalMesh);
+
+	RearDiffuser = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("RearDiffuser"));
+	RearDiffuser->SetupAttachment(CarSkeletalMesh);
+
+	WingMirrorLeft = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("WingMirrorLeft"));
+	WingMirrorLeft->SetupAttachment(CarSkeletalMesh);
+
+	WingMirrorRight = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("WingMirrorRight"));
+	WingMirrorRight->SetupAttachment(CarSkeletalMesh);
+
+	Wiper = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Wiper"));
+	Wiper->SetupAttachment(CarSkeletalMesh);
+
+	FrontWheelRight = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("FrontWheelRight"));
+	FrontWheelRight->SetupAttachment(CarSkeletalMesh);
+
+	FrontWheelLeft = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("FrontWheelLeft"));
+	FrontWheelLeft->SetupAttachment(CarSkeletalMesh);
+
+	FrontWheelBlur = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("FrontWheelBlur"));
+	FrontWheelBlur->SetupAttachment(CarSkeletalMesh);
+
+	RearWheelBlur = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("RearWheelBlur"));
+	RearWheelBlur->SetupAttachment(CarSkeletalMesh);
+
+	RearWheelRight = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("RearWheelRight"));
+	RearWheelRight->SetupAttachment(CarSkeletalMesh);
+
+	RearWheelLeft = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("RearWheelLeft"));
+	RearWheelLeft->SetupAttachment(CarSkeletalMesh);
+
+	FrontBrakeDiscRight = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("FrontBrakeDiscRight"));
+	FrontBrakeDiscRight->SetupAttachment(CarSkeletalMesh);
+
+	FrontBrakeDiscLeft = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("FrontBrakeDiscLeft"));
+	FrontBrakeDiscLeft->SetupAttachment(CarSkeletalMesh);
+
+	RearBrakeDiscRight = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("RearBrakeDiscRight"));
+	RearBrakeDiscRight->SetupAttachment(CarSkeletalMesh);
+
+	RearBrakeDiscLeft = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("RearBrakeDiscLeft"));
+	RearBrakeDiscLeft->SetupAttachment(CarSkeletalMesh);
+
+	FrontCaliperRight = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("FrontCaliperRight"));
+	FrontCaliperRight->SetupAttachment(CarSkeletalMesh);
+
+	FrontCaliperLeft = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("FrontCaliperLeft"));
+	FrontCaliperLeft->SetupAttachment(CarSkeletalMesh);
+
+	RearCaliperRight = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("RearCaliperRight"));
+	RearCaliperRight->SetupAttachment(CarSkeletalMesh);
+
+	RearCaliperLeft = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("RearCaliperLeft"));
+	RearCaliperLeft->SetupAttachment(CarSkeletalMesh);
+
+	Interior = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Interior"));
+	Interior->SetupAttachment(CarSkeletalMesh);
+
+	CockpitConsole = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("CockpitConsole"));
+	CockpitConsole->SetupAttachment(CarSkeletalMesh);
+
+	Net = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Net"));
+	Net->SetupAttachment(CarSkeletalMesh);
+
+	PedalAcceleration = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("PedalAcceleration"));
+	PedalAcceleration->SetupAttachment(CarSkeletalMesh);
+
+	PedalBrake = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("PedalBrake"));
+	PedalBrake->SetupAttachment(CarSkeletalMesh);
+
+	SeatNetClamps = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("SeatNetClamps"));
+	SeatNetClamps->SetupAttachment(CarSkeletalMesh);
+
+	SteeringWheel = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("SteeringWheel"));
+	SteeringWheel->SetupAttachment(CarSkeletalMesh);
+
+	EngineComponents = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("EngineComponents"));
+	EngineComponents->SetupAttachment(CarSkeletalMesh);
 }
 
 void APorscheCar::BeginPlay()
