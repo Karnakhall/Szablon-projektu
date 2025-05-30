b3a017faf9d81cd4917def598a8195cfaafd2a0b
Karnakhall <emil_q21@wp.pl>
2025-05-30 19:37:15 +0200

    Update PorscheCar.h

--- Source/Praktyki/Public/Cars/PorscheCar.h
+++ Source/Praktyki/Public/Cars/PorscheCar.h
@@ -1,9 +1,11 @@
 // Copyright 2025 Teyon. All Rights Reserved.
+// Main player car class - inherits from AWheeledVehiclePawn
 
 #pragma once
 
 #include "CoreMinimal.h"
 #include "WheeledVehiclePawn.h"
+#include "ChaosVehicleMovementComponent.h"
 #include "PorscheCar.generated.h"
 
 /**
